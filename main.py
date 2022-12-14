bd057cdcbab2915fa624ccc14a0c673753e71560
superpandachu666 <114162546+superpandachu666@users.noreply.github.com>
2022-12-14 12:20:31 +0100

    class

--- main.py
+++ main.py
@@ -2,30 +2,23 @@ import tkinter as tk
 from random import randrange
 from tkinter import messagebox
 import time
+import random
 
-class cellule:
-    def __init__(self):
-        self.vivant = ""
-        self.mort = ""
+class Cellule:
+    def __init__(self, etat, age):
+        self.etat = etat # "Vivante" ou "Morte"
+        self.age = random.randint(1, 10)
+        self.nouvel_etat = self.etat
+    def update(self, self.etat):
+    if etat > 0 :
+        etat = self.etat += 1
 
-def plateauJeu():
-    # Create a 10x10 grid of labels
-    labels = [[tk.Label(root) for _ in range(10)] for _ in range(10)]
-    # Add the labels to the grid
-    for i in range(10):
-        for j in range(10):
-            labels[i][j].grid(row=i, column=j)
+    elif etat > 10 :
+        print("La cellule est morte")
+    else:
+        etat = self.etat += 1
 
-    # Function to update the grid every second
-    def update_grid():
-        for i in range(10):
-            for j in range(10):
-                # Update the text of each label to add 100
-                labels[i][j].config(text=int(labels[i][j]["text"]) + 100)
-        # Call this function again after 1 second
-        root.after(1000, update_grid)
-    # Start updating the grid
-    update_grid()
+
 
 #On crée la fenêtre principale
 root = tk.Tk()
