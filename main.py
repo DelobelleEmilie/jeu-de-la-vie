0a27d920e20f97ab11c8b8fea734ffea1beca4f8
Rémy Vissyrias <remy.vissyrias@gmail.com>
2022-12-15 11:32:49 +0100

    Fix classes

--- main.py
+++ main.py
@@ -4,26 +4,76 @@ from tkinter import messagebox
 import time
 import random
 
+
 class Cellule:
-    def __init__(self, etat, age):
-        self.etat = etat # "Vivante" ou "Morte"
-        self.age = random.randint(1, 10)
-        self.nouvel_etat = self.etat
-    def update(self, self.etat):
-    if etat > 0 :
-        etat = self.etat += 1
+    alive: bool
+    age: int
 
-    elif etat > 10 :
-        print("La cellule est morte")
-    else:
-        etat = self.etat += 1
+    def __init__(self, alive: bool, age: int):
+        self.alive = alive  # "Vivante" ou "Morte"
+        self.age = age
+
+    def __str__(self):
+        alive = ("Vivante", "Morte")[self.alive]
+        return f"[{alive} - {self.age}]"
+
+    def add_age(self, value: int):
+        if self.age < 1:
+            self.age += 1
+            return
+
+        self.age += value
+
+        if self.age >= 10:
+            self.age = -5
+            self.alive = False
+
+    def born(self):
+        self.age = 1
+        self.alive = True
+
+    def get_age(self):
+        return self.age
+
+    def is_alive(self):
+        return self.alive
+
+
+class GameBoard:
+    size: int
+    cellules: []
+
+    def __init__(self, size: int, ratio: float):
+        self.size = size
+        self.cellules = []
+        for x in range(self.size):
+            self.cellules.append([])
+            for y in range(self.size):
+                alive = random.random() > (1 - ratio)
+                self.cellules[x].append(Cellule(alive, (1, 0)[alive]))
+
+    def print(self):
+        alive = 0
+        dead = 0
+        for x in range(self.size):
+            for y in range(self.size):
+                cell: Cellule = self.cellules[x][y]
+                print(f"({x},{y}) - {cell}")
+                if (cell.alive):
+                    alive += 1
+                else:
+                    dead += 1
+        print(f"{alive} vivantes et {dead} mortes")
 
 
 
-#On crée la fenêtre principale
+# On crée la fenêtre principale
 root = tk.Tk()
 root.title("Jeu de la vie")
-root.geometry("300x300")
+root.geometry("500x500")
+
+board = GameBoard(10, 0.4)
+board.print()
 
 # Create an image object using the PhotoImage class
 image = tk.PhotoImage(file="background.png")
@@ -33,14 +83,13 @@ label = tk.Label(root, image=image)
 # to position it at the center of the window
 label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-
-button = tk.Button(root, text="Démarrer le jeu", command=plateauJeu)
-button.pack()
+# button = tk.Button(root, text="Démarrer le jeu", command=plateauJeu)
+# button.pack()
 
 menubar = tk.Menu(root)
 filemenu = tk.Menu(menubar, tearoff=0)
 filemenu.add_command(label="Redémarrer la partie", )
-filemenu.add_command(label="Quitter la partie",command=exit )
+filemenu.add_command(label="Quitter la partie", command=exit)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", )
 menubar.add_cascade(label="File", )
