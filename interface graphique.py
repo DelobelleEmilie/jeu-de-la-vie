af0188771dfe520ff1af751b705c4e039a664083
superpandachu666 <114162546+superpandachu666@users.noreply.github.com>
2022-12-16 13:48:27 +0100

    int graph

--- interface graphique.py
+++ interface graphique.py
@@ -3,8 +3,7 @@ from tkinter import *
 
 # Create object
 root = Tk()
-def create():
-    win = Toplevel(root)
+page1=Tk()
 
 # Adjust size
 root.geometry("400x400")
@@ -19,14 +18,30 @@ label1.place(x = 0, y = 0)
 label2 = Label( root, text = "Welcome")
 label2.pack(anchor= CENTER)
 
-# Create Frame
+
+def create1():
+    win = Toplevel(root)
+    frame2 = win
+    frame2.pack(anchor= CENTER)
+    LancerBouton1 = Button(win, text="Valider la taille du plateau de base",command = create )
+    LancerBouton1.pack(anchor= CENTER)
+
+    # Add image file
+    bg = PhotoImage(file = "background2.png")
+
+    # Show image using label
+    label1 = Label( win, image = bg)
+    label1.place(x = 0, y = 0)
+
+    label2 = Label( win, text = "Welcome")
+    label2.pack(anchor= CENTER)
+    # Create Frame
 frame1 = Frame(root)
 frame1.pack(anchor= CENTER)
-verifierBouton = Button(root, text="Valider la taille du plateau de base", )
+verifierBouton = Button(root, text="Valider la taille du plateau de base",command = create1 )
 verifierBouton.pack(anchor= CENTER)
 motdepasseEntry = Entry(root)
 motdepasseEntry.pack(anchor= CENTER)
-
 
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
@@ -42,5 +57,6 @@ helpmenu.add_command(label="About...")
 menubar.add_cascade(label="Help", menu=helpmenu)
 
 root.config(menu=menubar)
+
 # Execute tkinter
 root.mainloop()
