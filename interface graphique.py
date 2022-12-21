d30bb5741cdc2f81b3cb3f2b03877503d57e7251
superpandachu666 <114162546+superpandachu666@users.noreply.github.com>
2022-12-21 21:16:19 +0100

    int graph

--- interface graphique.py
+++ interface graphique.py
@@ -3,48 +3,32 @@ from tkinter import *
 
 # Create object
 root = Tk()
-page1=Tk()
+page1 = Tk()
 
 # Adjust size
 root.geometry("400x400")
 
 # Add image file
-bg = PhotoImage(file = "background.png")
+bg = PhotoImage(file="background.png")
 
 # Show image using label
-label1 = Label( root, image = bg)
-label1.place(x = 0, y = 0)
+label1 = Label(root, image=bg)
+label1.place(x=0, y=0)
 
-label2 = Label( root, text = "Welcome")
-label2.pack(anchor= CENTER)
+label2 = Label(root, text="Welcome")
+label2.pack(pady=50)
 
-canvas = tk.Canvas(window, width=400, height=400)
-canvas.pack()
-canvas.create_carre(x, x,command = couleur, fill="red")
-
-def create1():
-    win = Toplevel(root)
-    frame2 = win
-    frame2.pack(anchor= CENTER)
-    LancerBouton1 = Button(win, text="Valider la taille du plateau de base",command = create )
-    LancerBouton1.pack(anchor= CENTER)
-
-    # Add image file
-    bg = PhotoImage(file = "background2.png")
-
-    # Show image using label
-    label1 = Label( win, image = bg)
-    label1.place(x = 0, y = 0)
-
-    label2 = Label( win, text = "Welcome")
-    label2.pack(anchor= CENTER)
-    # Create Frame
+# Create Frame
 frame1 = Frame(root)
-frame1.pack(anchor= CENTER)
-verifierBouton = Button(root, text="Valider la taille du plateau de base",command = create1 )
-verifierBouton.pack(anchor= CENTER)
-motdepasseEntry = Entry(root)
-motdepasseEntry.pack(anchor= CENTER)
+frame1.pack(pady=20)
+
+
+def create():
+    win = Toplevel(root)
+
+
+verifierBouton = Button(root, text="Commencer le jeu", command=create)
+verifierBouton.pack(anchor=CENTER)
 
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
@@ -60,7 +44,6 @@ helpmenu.add_command(label="About...")
 menubar.add_cascade(label="Help", menu=helpmenu)
 
 root.config(menu=menubar)
-
 
 # Execute tkinter
 root.mainloop()
--- logique.py
+++ logique.py
@@ -1,16 +1,10 @@
-import tkinter as tk
-from random import randrange
-from tkinter import messagebox
-import time
 import random
-
-paused = False
 
 
 class Cellule:
-    #valeur vrai faux
+    # valeur vrai faux
     alive: bool
-    #valeur entier
+    # valeur entière
     age: int
 
     def __init__(self, alive: bool, age: int):
@@ -21,8 +15,18 @@ class Cellule:
         alive = ("Vivante", "Morte")[self.alive]
         return f"[{alive} - {self.age}]"
 
-    #gérer quand la cellule est morte
-    #ajout d'âge
+    def update(self):
+        # On tue la cellule si elle est trop agée (10 ans).
+        if self.age >= 10:
+            # age négatif parce qu'on doit compter 5 tours avant de la faire renaitre
+            self.age = -5
+            self.alive = False
+
+        if self.age > 0 and not self.alive:
+            self.born()
+
+    # gérer quand la cellule est morte
+    # ajout d'âge
     def add_age(self, value: int):
         if self.age < 1:
             self.age += 1
@@ -30,44 +34,76 @@ class Cellule:
 
         self.age += value
 
-        if self.age >= 10:
-            # age négatif parce qu'on doit compter 5 tours avant de la faire renaitre
-            self.age = -5
-            self.alive = False
-
-    #fonction naissance
+    # fonction naissance
     def born(self):
         self.age = 1
         self.alive = True
 
-    #fonction obtention d'âge
+    # fonction obtention d'âge
     def get_age(self):
         return self.age
 
-    #fonction de vie
+    # fonction de vie
     def is_alive(self):
         return self.alive
 
+    def get_color(self):
+        if self.age > 9:
+            # La cellule est âgée de plus de 9 ans, couleur rouge
+            return "red"
+        elif self.age > 3:
+            # La cellule est âgée entre 4 et 8 ans, couleur bleue
+            return "blue"
+        elif self.age > 0:
+            # La cellule est âgée entre 1 et 3 ans, couleur jaune
+            return "yellow"
+        else:
+            # La cellule est âgée de moins de 1 an, couleur par défaut
+            return "white"
 
-#damier
+
+# damier
 class GameBoard:
     size: int
     cellules: []
+    paused: bool
 
     def __init__(self, size: int, ratio: float):
         self.size = size
         self.cellules = []
+        self.paused = False
         for x in range(self.size):
             self.cellules.append([])
             for y in range(self.size):
                 # (1 -=bool (si ratio = 0.4, 40% de chance d'être True) ratio)
-                #alive = variable
-                #random.random = nombre entre 0 et 1
-                alive = random.random() > \(1 - ratio)
-                #enregistre les valeurs dans un tableau
+                # alive = variable
+                # random.random = nombre entre 0 et 1.
+                alive = random.random() > (1 - ratio)
+                # enregistre les valeurs dans un tableau
                 self.cellules[x].append(Cellule(alive, (1, 0)[alive]))
 
-    #chere la taille du tableau
+    def set_pause(self, paused: bool):
+        self.paused = paused
+
+    def update(self):
+        for x in range(self.size):
+            for y in range(self.size):
+                voisines_vivantes = 0
+                for i in range(x - 1, x + 2):
+                    for j in range(y - 1, y + 2):
+                        if 0 <= i < self.size and 0 <= j < self.size and (i != 0 or j != 0):
+                            if self.cellules[i][j].is_alive():
+                                voisines_vivantes += 1
+                age = 1
+                if voisines_vivantes > 2:
+                    age += voisines_vivantes - 2
+                self.cellules[x][y].add_age(age)
+
+        for x in range(self.size):
+            for y in range(self.size):
+                self.cellules[x][y].update()
+
+    # chere la taille du tableau
     def print(self):
         alive = 0
         dead = 0
@@ -75,51 +111,8 @@ class GameBoard:
             for y in range(self.size):
                 cell: Cellule = self.cellules[x][y]
                 print(f"({x},{y}) - {cell}")
-                if (cell.alive):
+                if cell.is_alive():
                     alive += 1
                 else:
                     dead += 1
         print(f"{alive} vivantes et {dead} mortes")
-
-def create():
-    win = Toplevel(root)
-
-def AjoutCase():
-    for x in range(self.size):
-        for y in range(self.size):
-            voisines_vivantes = 0;
-            for i in range(x - 1, x + 2):
-                for j in range(y - 1, y + 2):
-                    if i >= 0 and i < size and j >= 0 and j < sizeand (i != 0 or j != 0):
-                        if grid[i][j].is_alive():
-                            voisines_vivantes += 1
-            age = 1;
-            if voisines_vivantes > 2:
-                age += voisines_vivantes - 2;
-            grid[x][y].add_age(age)
-
-
-
-def Couleur():
-    if cell.years > 9:
-        # La cellule est âgée de plus de 9 ans, couleur rouge
-        color = "red"
-    elif cell.years > 3:
-        # La cellule est âgée entre 4 et 8 ans, couleur bleue
-        color = "blue"
-    elif cell.years > 0:
-        # La cellule est âgée entre 1 et 3 ans, couleur jaune
-        color = "yellow"
-    else:
-        # La cellule est âgée de moins de 1 an, couleur par défaut
-        color = "default"
-
-
-def Pause():
-    global paused
-    paused = True
-
-def Unpause():
-    global paused
-    paused = False
-
