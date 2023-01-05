cf26d33e53b4a0c7e41c19cbf935af9d437fed70
superpandachu666 <114162546+superpandachu666@users.noreply.github.com>
2023-01-05 22:04:53 +0100

    test

--- interface_graphique.py
+++ interface_graphique.py
@@ -9,7 +9,7 @@ class GameWindow:
     board: GameBoard
     canvas: tk.Canvas
 
-    def __init__(self, board: GameBoard):
+    def __init__(self, board: GameBoard,start,paused):
         self.board = board
         self.root = tk.Tk()
         self.root.geometry("800x800")
@@ -18,6 +18,28 @@ class GameWindow:
         self.draw()
         self.root.after(1000, self.loop)
         self.root.mainloop()
+        self.menubar = tk.Menu(self.root)
+        self.filemenu = tk.Menu(self.menubar , tearoff=0)
+        self.filemenu.add_command(label="Redémarrer la partie", command=self.restart)
+        self.filemenu.add_command(label="Quitter la partie", command=exit)
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label="Exit", )
+        self.menubar.add_cascade(label="File", )
+        self.helpmenu = tk.Menu(self.menubar , tearoff=0)
+        self.helpmenu.add_command(label="Régles", command=self.regles)
+        self.menubar.add_cascade(label="Help", menu=self.helpmenu)
+
+        self.pausemenu = tk.Menu(self.menubar , tearoff=0)
+        self.pausemenu.add_command(label="Pause", command=self.pause)
+        self.menubar.add_cascade(label="Pause", menu=self.helpmenu)
+        self.control_menu = tk.Menu(self.menubar )
+
+        self.menu_bar.add_cascade(label='Control', menu=self.control_menu)
+        self.pause_button = tk.MenuButton(self.control_menu, label='Pause', command=pause)
+        self.control_menu.add_command(label='Pause', command=pause)
+
+        self.control_menu.add_command(label='Start', command=start)
+        self.root.config(menu=self.menubar )
 
     def draw(self):
         cell_size = 800 / self.board.size
@@ -33,6 +55,21 @@ class GameWindow:
         self.board.update()
         self.draw()
         self.root.after(1000, self.loop)
+
+    def start(self):
+        # réactivez le bouton pause ici
+        self.pause_button.configure(state='normal')
+
+    def pause(self):
+        # désactivez le bouton pause ici
+        self.pause_button.configure(state='disabled')
+
+    def on_mouse_click(self, event):
+        # vérifiez si le bouton pause est enfoncé ici
+        if self.pause_button.is_pressed():
+            self.pause()
+            # changez l'état de la cellule ici
+            self.cell.state = not self.cell.state
 
 class SlideBar():
 
